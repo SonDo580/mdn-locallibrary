d4ff67bf5b38b9643fe4db12163590057a2f0ef8
Son Do <dominhson580@gmail.com>
2022-11-10 22:34:18 +0700

    Add 2 virtual properties

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
 
 const Schema = mongoose.Schema;
 
@@ -24,6 +25,18 @@ AuthorSchema.virtual("name").get(function () {
 // Virtual for author's URL
 AuthorSchema.virtual("url").get(function () {
   return `/catalog/author/${this._id}`;
+});
+
+AuthorSchema.virtual("date_of_birth_formatted").get(function () {
+  return DateTime.fromJSDate(this.date_of_birth).toLocaleString(
+    DateTime.DATE_MED
+  );
+});
+
+AuthorSchema.virtual("date_of_death_formatted").get(function () {
+  return DateTime.fromJSDate(this.date_of_death).toLocaleString(
+    DateTime.DATE_MED
+  );
 });
 
 module.exports = mongoose.model("Author", AuthorSchema);
