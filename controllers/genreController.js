149273e1a98c8fecfe97e916067c5c3d3fc45a4c
Son Do <dominhson580@gmail.com>
2022-11-10 23:12:03 +0700

    Get genre details and books

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -19,8 +19,35 @@ exports.genre_list = (req, res, next) => {
 };
 
 // Display detail page for a specific Genre.
-exports.genre_detail = (req, res) => {
-  res.send(`NOT IMPLEMENTED: Genre detail: ${req.params.id}`);
+exports.genre_detail = (req, res, next) => {
+  async.parallel(
+    {
+      genre(callback) {
+        Genre.findById(req.params.id).exec(callback);
+      },
+
+      genre_books(callback) {
+        Book.find({ genre: req.params.id }).exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+
+      if (results.genre === null) {
+        const err = new Error("Genre not found");
+        err.status = 404;
+        return next(err);
+      }
+
+      res.render("genre_detail", {
+        title: "Genre Detail",
+        genre: results.genre,
+        genre_books: results.genre_books,
+      });
+    }
+  );
 };
 
 // Display Genre create form on GET.
