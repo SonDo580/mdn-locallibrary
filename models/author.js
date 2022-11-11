989d62867cef6f2be400fb0108745a1bf4d4c116
Son Do <dominhson580@gmail.com>
2022-11-11 22:38:33 +0700

    Display '???' if the date is not provided

--- models/author.js
+++ models/author.js
@@ -30,13 +30,13 @@ AuthorSchema.virtual("url").get(function () {
 AuthorSchema.virtual("date_of_birth_formatted").get(function () {
   return this.date_of_birth
     ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED)
-    : "";
+    : "???";
 });
 
 AuthorSchema.virtual("date_of_death_formatted").get(function () {
   return this.date_of_death
     ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)
-    : "";
+    : "???";
 });
 
 module.exports = mongoose.model("Author", AuthorSchema);
