04556167652a18d69483ecc759c48d07a05288f4
Son Do <dominhson580@gmail.com>
2022-11-12 15:16:54 +0700

    Render the genre_form.pug view

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -54,7 +54,7 @@ exports.genre_detail = (req, res, next) => {
 
 // Display Genre create form on GET.
 exports.genre_create_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: Genre create GET");
+  res.render("genre_form", { title: "Create Genre" });
 };
 
 // Handle Genre create on POST.
