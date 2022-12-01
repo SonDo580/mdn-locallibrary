2f721a377d376573c1e211d47544a930ee4e9b52
Son Do <dominhson580@gmail.com>
2022-12-01 23:24:44 +0700

    Validate and sanitize input

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -56,9 +56,34 @@ exports.author_create_get = (req, res) => {
 };
 
 // Handle author create on POST
-exports.author_create_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author create POST");
-};
+exports.author_create_post = [
+  // Validate and sanitize input
+  body("first_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("First name must be specified")
+    .isAlphanumeric()
+    .withMessage("First name has non-alphanumeric characters"),
+
+  body("family_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Family name must be specified")
+    .isAlphanumeric()
+    .withMessage("Family name has non-alphanumeric characters"),
+
+  body("date_of_birth", "Invalid date of birth")
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+
+  body("date_of_death", "Invalid date of death")
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+];
 
 // Display Author delete form on GET.
 exports.author_delete_get = (req, res) => {
