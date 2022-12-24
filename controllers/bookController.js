9d4c86b2fd830b24cdbdf3835f693a455719c89a
Son Do <dominhson580@gmail.com>
2022-12-24 21:13:26 +0700

    Validate and sanitize inputs

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -264,4 +264,17 @@ exports.book_update_post = [
     }
     next();
   },
+
+  // Validate and sanitize fields
+  body("title", "Title must not be empty").trim().isLength({ min: 1 }).escape(),
+  body("author", "Author must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("summary", "Summary must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("isbn", "ISBN must not be empty").trim().isLength({ min: 1 }).escape(),
+  body("genre.*").escape(),
 ];
