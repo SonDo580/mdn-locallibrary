d0020cf4a05a434c631b17b72304d7722e0f7b89
Son Do <dominhson580@gmail.com>
2022-12-25 22:00:24 +0700

    Prepare for deployment

--- app.js
+++ app.js
@@ -12,8 +12,9 @@ const app = express();
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
-const mongoDB =
+const dev_db_url =
   "mongodb+srv://mdn-library:mdn-library@cluster0.affchzl.mongodb.net/?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
